edebe961c1efa8b377c0f5e8b139b8db577606ec
Pablo Marino <pmarino2013@gmail.com>
2025-12-30 10:30:34 -0300

    Agrego README

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const MONGO_URI =
-  "mongodb+srv://user:rE2uUqjeUSmxwYew@devtuc0.rpurqjb.mongodb.net/test";
+  "mongodb+srv://user:_tu_password@_tu_base_de_datos.mongodb.net";
 
 export const dbConnect = async () => {
   try {
